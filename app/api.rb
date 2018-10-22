6d7bdb0aabe7db1cab8e549a61f890f76d15cd7f
imontalvomiguel <ubaldo.im@gmail.com>
2018-10-22 07:04:28 -0500

    Handleing failure

--- app/api.rb
+++ app/api.rb
@@ -1,7 +1,15 @@
 module ExpenseTracker
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
     post '/expenses' do
-      json :expense_id => 23
+      request.body.rewind
+      data = JSON.parse request.body.read
+      result = @ledger.record(data)
+      json :expense_id => result.expense_id
     end
 
     get '/expenses/:date' do
