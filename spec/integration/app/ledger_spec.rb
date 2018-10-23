de0e794cc1986d8853689fd07d20653241564dcb
imontalvomiguel <ubaldo.im@gmail.com>
2018-10-22 21:45:58 -0500

    Save expenses

--- spec/integration/app/ledger_spec.rb
+++ spec/integration/app/ledger_spec.rb
@@ -2,16 +2,30 @@ require_relative '../../support/db'
 require_relative '../../../lib/ledger'
 
 module ExpenseTracker
-  RSpec.describe Ledger do
+  RSpec.describe Ledger, :aggregate_failures  do
     let(:ledger) { Ledger.new }
     let(:expense) do
       {
-        'payee' => 'Startucks',
+        'payee' => 'Starbucks',
         'amount' => 5.75,
         'date' => '2017-06-10'
       }
     end
 
-    describe '#record'
+    describe '#record' do
+      context 'with a valid expense' do
+        it 'successfully saves the expense in the DB' do
+          result = ledger.record(expense)
+
+          expect(result).to be_success
+          expect(DB[:expenses].all).to match [a_hash_including(
+            id: result.expense_id,
+            payee: 'Starbucks',
+            amount: 5.75,
+            date: Date.iso8601('2017-06-10')
+          )]
+        end
+      end
+    end
   end
 end
